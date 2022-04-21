b6ad88e9878037c37a1c2442139d7eadb4e1a374
lsaac <2423860442@qq.com>
2022-04-21 19:47:10 +0800

    初始化项目

---
+++ Messager/Controllers/ConversationsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ConversationsViewController.swift
+//  Messager
+//
+//  Created by lsaac on 2022/4/21.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+        
+    }
+    
+
+
+
+}
